c719ea3d39d79787d406e586ccb7504d4b99da12
CraftBukkit <craftbukkit@github.com>
2013-06-02 15:10:56 +1000

    Skeleton API Implementations
    This contains the basic, empty implementations for some Spigot-API extensions. They are included early in the patching progress so that compilation will still succeed midway despite the APIs only being provided by subsequent patches.
    (md_5 <md_5@live.com.au>)

--- server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
+++ server/src/main/java/org/bukkit/craftbukkit/entity/CraftPlayer.java
@@ -1902,4 +1902,15 @@ public class CraftPlayer extends CraftHumanEntity implements Player {
     public boolean isAllowingServerListings() {
         return getHandle().allowsListing();
     }
+
+    // Spigot start
+    private final Player.Spigot spigot = new Player.Spigot()
+    {
+    };
+
+    public Player.Spigot spigot()
+    {
+        return spigot;
+    }
+    // Spigot end
 }
